535e23a09d047b1bc8122c53bb5a81dcb13264e9
cxq <caoxiaoqiang@xiaomi.com>
2019-08-23 18:09:10 +0800

    conf

--- gen_pretrain_data.py
+++ gen_pretrain_data.py
@@ -1,1 +1,23 @@
 # -*- coding:utf-8 -*-
+import yaml
+
+
+def main():
+    pass
+
+
+def load_conf(path):
+    return yaml.load(open(path))
+
+
+def some_test():
+    conf = load_conf("./conf/gen_pretrain_data.yaml")
+    print(conf)
+    print(conf["input_files"])
+    # x = "xxx,yyy"
+    # y = x.strip().split(",")
+    # print(y)
+
+
+if __name__ == '__main__':
+    some_test()
